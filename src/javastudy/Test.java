af139ce46b0fcd7bdf44b9d6fc50a38dedce9e09
DESKTOP-VMUUAM3\Yookmoonsu <yookmoon@naver.com>
2017-11-28 14:21:30 +0900

    스캐너 예제 실습

--- src/javastudy/Test.java
+++ src/javastudy/Test.java
@@ -1,11 +1,23 @@
 package javastudy;
 
+import java.util.*;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("hello java");
+		Test test = new Test();
+		test.scanner();
 	}
-
+	
+	public void scanner() {
+		System.out.println("hello java");
+		
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		int number = Integer.parseInt(input);
+		
+		System.out.println(number);
+		scanner.close();
+	}
 }
